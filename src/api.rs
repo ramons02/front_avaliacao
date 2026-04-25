80ac3a380e4e3919efd790113c435d362d78bf3b
Ramon Silva <ramonsillva70@gmail.com>
2026-04-25 16:50:27 -0300

    fix: alterando BASE url

--- src/api.rs
+++ src/api.rs
@@ -2,7 +2,7 @@ use gloo_net::http::Request;
 use crate::auth::obter_token;
 use crate::models::{Avaliacao, LoginRequest, LoginResponse, Paciente};
 
-const BASE: &str = "https://fisiopower-back-production.up.railway.app";
+const BASE: &str = "https://fisiopower-back-production.up.railway.app/api/v1";
 
 fn bearer() -> String {
     format!("Bearer {}", obter_token().unwrap_or_default())
